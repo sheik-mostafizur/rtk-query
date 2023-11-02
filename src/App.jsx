85cb67a2ea04a5fc64769e38cc50f8c68f06ee1c
sheik-mostafizur <sheikmostafizur2001@gmail.com>
2023-11-02 19:49:23 +0600

    counter using redux

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
+import {useSelector} from "react-redux";
+import {Counter} from "./component/counter";
+
 function App() {
+  const {value: counterState} = useSelector((state) => state.counter);
+
   return (
     <>
       <h1>Hello RTK Query</h1>
+      <Counter />
+      <h2>Counter Value: {counterState}</h2>
     </>
   );
 }
